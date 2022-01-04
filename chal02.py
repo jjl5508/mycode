1b1fc5bb4f791987ed167e036366229e7bfaaf06
jjl5508 <jjl5508@users.noreply.github.com>
2022-01-04 18:09:13 +0000

    learning about dictionaries

---
+++ chal02.py
@@ -0,0 +1,7 @@
+icecream = ["indentation", "spaces"]
+tlgstudents= ["Aaron","Alex","Alonzo","Brandon","Chris","Francisco","James","Jonathan","Lillian","Manuel","Patrick","Robert","Ryan","Troy","Wes","Henry","Yalined"]
+icecream.append(4)
+print(icecream)
+choice = int(input("Choose a number!"))
+student_name = tlgstudents[choice]
+print(f"{student_name} always uses {icecream} to indent")
---
+++ chal05.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+char_name = input("Which character do you want to know about?(Flash,Batman, Superman): ")
+char_stat = input("What statistic do you wnat to know about? (strength, speed, or intelligence): ")
+char_value =  {"flash":{"speed": "fastest", "intelligence": "lowest", "strength": "lowest"}, "batman":{"speed": "slowest", "intelligence": "Highest", "strength": "Money"}, "superman":{"speed": "fast", "intelligence": "average", "strength": "strongest"}}
+if "flash" == char_value[0]
+    then 
+        print(
